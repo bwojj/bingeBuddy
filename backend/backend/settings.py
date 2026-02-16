a3135086e9a9775162412815d0212f58e569d195
Blake <wojobeemer@Blakes-MacBook-Pro-2.local>
2026-02-16 10:26:17 -0500

    Added custom tokenobtain and refresh views from simplejwt for secure cookie auth

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework', 
+    'bingeBuddyAPI',
 ]
 
 MIDDLEWARE = [
@@ -98,13 +100,19 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
--- backend/bingeBuddyAPI/views.py
+++ backend/bingeBuddyAPI/views.py
@@ -1,3 +1,94 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-# Create your views here.
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs): 
+        try: 
+            # submits post request with the given data
+            response = super().post(request, *args, **kwargs)
+
+            # gets the access and refresh tokens from request
+            tokens = response.data 
+
+            # saves the access and refresh token from json returned
+            access_token = tokens['access']
+            refresh_token = tokens['refresh']
+
+            # inits response 
+            res = Response()
+
+            # sets the response to succeeding, gives both tokens
+            res.data = {
+                'success': True, 
+                'access': access_token, 
+                'refresh': refresh_token, 
+            }
+
+            # sets the cookies, for secure auth
+            res.set_cookie(
+                key='access_token', 
+                value=access_token,
+                httponly=True, 
+                secure=True, 
+                samesite='None',
+                path='/',
+            )
+            res.set_cookie(
+                key='refresh_token',
+                value=refresh_token,
+                httponly=True, 
+                secure=True, 
+                samesite='None',
+                path='/', 
+            )
+            return res
+        # if post fails 
+        except: 
+            return Response({'success': False})
+
+class CustomTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs): 
+        try:
+            # gets the refresh token cookie
+            refresh_token = request.COOKIES.get('refresh_token')
+
+            # makes sure refresh token exists
+            if not refresh_token:
+                return Response({'refreshed': False, 'error': 'No refresh token'})
+
+            # gets the refresh token, and sets it to the cookie
+            request.data['refresh'] = refresh_token 
+
+            # calls post method 
+            response = super().post(request, *args, **kwargs)
+
+            #gets tokens 
+            tokens = response.data 
+            access_token = tokens['access']
+
+            # inits response 
+            res = Response()
+
+            # sets the response to signify successful refresh 
+            res.data = {
+                'refreshed': True, 
+                'access': access_token
+            }
+
+            # sets new refreshed cookie
+            res.set_cookie(
+                key="access_token", 
+                value=access_token, 
+                httponly=True, 
+                secure=True, 
+                samesite="None", 
+                path="/"
+            )
+
+            return res
+        except: 
+            return Response({'refreshed': False})

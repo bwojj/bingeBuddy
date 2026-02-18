8377daf05ac8aa5431c22148fa11c625f6224d6c
Blake <wojobeemer@Blakes-MacBook-Pro-2.local>
2026-02-17 21:57:50 -0500

    Added dynamic route loading based on api call to isauthenticated

--- frontend/app/_layout.jsx
+++ frontend/app/_layout.jsx
@@ -1,12 +1,22 @@
 import {Stack, Redirect } from 'expo-router'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { authenticated } from '../components/AuthApi.js'
 
 const RootLayout = () => {
 
+    // defines state for if user is authenticated
     const [isAuthenticated, setIsAuthenticated] = useState(false); 
 
-    setIsAuthenticated(authenticated); 
+    // update state on mount 
+    useEffect(() => {
+        // get result from authenticated API call, set authenticated based on
+        const checkAuth = async () => {
+            const auth = await authenticated(); 
+            setIsAuthenticated(auth); 
+        }
+
+        checkAuth(); 
+    }, [])
 
     return(
         <Stack screenOptions={{ headerShown: false }}>
--- frontend/components/AuthApi.js
+++ frontend/components/AuthApi.js
@@ -1,5 +1,4 @@
 
-const axios = require('axios'); 
 const BASEURL = 'http://127.0.0.1:8000/';
 
 export const authenticated = async () => {

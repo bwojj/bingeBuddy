70265fc713cd3d3b20b82af93f75052ebb794ef3
Blake <wojobeemer@Blakes-MacBook-Pro-2.local>
2026-07-09 12:58:58 -0400

    feat: add error handling to barebones coach endpoint

--- backend/bingeBuddyAPI/views.py
+++ backend/bingeBuddyAPI/views.py
@@ -471,38 +471,41 @@ def delete_account(request):
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def ai_coach(request):
-    # defines LLM 
-    llm = ChatGoogleGenerativeAI(
-        model='gemini-2.5-flash',
-        temperature=0.7,
-        google_api_key=os.environ.get('GOOGLE_API_KEY')
-    )
+    try: 
+        # defines LLM 
+        llm = ChatGoogleGenerativeAI(
+            model='gemini-2.5-flash',
+            temperature=0.7,
+            google_api_key=os.environ.get('GOOGLE_API_KEY')
+        )
 
-    # defines system prompt 
-    system_prompt = SystemMessagePromptTemplate.from_template(
-        """
-            You are an AI binge eating coach placed within a Binge Eating Recovery app, that 
-            is focused on the ‘beating the urge’ mindset, meaning the main vehicle to stop users 
-            from binge eating is to help them stop the urge. The main components are all mindset 
-            related. Users might ask questions for reassurance, help to beat an urge, or more. The 
-            mindset shifts you need to point to include the following: understand that the user has 
-            full control, the user only eats as a conscious decision, the binge eating goes against 
-            the users goal, the user should NOT refer to themselves as a binge eater. Beyond that be 
-            a helpful, and very sympathetic therapist for the user. Always be 100% kind. 
-        """
-    )
+        # defines system prompt 
+        system_prompt = SystemMessagePromptTemplate.from_template(
+            """
+                You are an AI binge eating coach placed within a Binge Eating Recovery app, that 
+                is focused on the beating the urge mindset, meaning the main vehicle to stop users 
+                from binge eating is to help them stop the urge. The main components are all mindset 
+                related. Users might ask questions for reassurance, help to beat an urge, or more. The 
+                mindset shifts you need to point to include the following: understand that the user has 
+                full control, the user only eats as a conscious decision, the binge eating goes against 
+                the users goal, the user should NOT refer to themselves as a binge eater. Beyond that be 
+                a helpful, and very sympathetic therapist for the user. Always be 100% kind. 
+            """
+        )
 
-    # defines user prompt from request
-    user_prompt = HumanMessagePromptTemplate(content=request.data.get('message'))
+        # defines user prompt from request
+        user_prompt = HumanMessagePromptTemplate(content=request.data.get('message'))
 
-    # defines full prompt template to use 
-    prompt = ChatPromptTemplate.from_messages([system_prompt, user_prompt])
+        # defines full prompt template to use 
+        prompt = ChatPromptTemplate.from_messages([system_prompt, user_prompt])
 
-    # defines chain for the llm 
-    chain = prompt | llm 
+        # defines chain for the llm 
+        chain = prompt | llm 
 
-    # defines output model gets
-    output = chain.invoke({})
+        # defines output model gets
+        output = chain.invoke({})
 
-    # returns content of the message
-    return Response({"ai-message": output.content})
+        # returns content of the message
+        return Response({"ai-message": output.content})
+    except Exception as e:
+        return Response({'error': e})
